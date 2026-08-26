ad97a9bbb7fa498fac5a5e117754ee19b2de8489
AbderrahimJami <abderrahimjami@outlook.com>
2026-08-26 23:07:49 +0100

    puzzle 3 key: no hidden-until-solved flags, just a pickup

--- Assets/Scripts/Interactables/Puzzles/Puzzle3/Puzzle3Key.cs
+++ Assets/Scripts/Interactables/Puzzles/Puzzle3/Puzzle3Key.cs
@@ -1,20 +1,12 @@
-using System;
 using UnityEngine;
 
 namespace Interactables.Puzzles.Puzzle3
 {
     public class Puzzle3Key : MonoBehaviour, InteractionInterface
     {
-        [SerializeField] private GameObject workingClockPrefab;
-
-        private void Start()
-        {
-            throw new NotImplementedException();
-        }
-
         public void Interact(GameObject interactor)
         {
-            Debug.Log("Hello World");
+            Destroy(gameObject);
         }
     }
 }
